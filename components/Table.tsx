e7f6ca0a1d41442687fe0cf3ef0b1773b28c74a8
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2023-03-18 13:07:57 -0300

    improve filters section

--- components/Table.tsx
+++ components/Table.tsx
@@ -13,6 +13,13 @@ import Select, { MultiValue } from 'react-select';
 import { Listing } from '@/utils/types';
 import { Provider, providers } from '@/data/providers';
 import { Tags, tags } from '@/data/tags';
+import {
+  FunnelIcon,
+  ArrowPathIcon,
+  XMarkIcon,
+} from '@heroicons/react/20/solid';
+
+import { Button, Icon } from '@tremor/react';
 
 type TableProps = {
   listings: Listing[];
@@ -23,6 +30,7 @@ const Table: React.FC<TableProps> = ({ listings }) => {
   const [selectedProvider, setSelectedProvider] =
     useState<MultiValue<Provider>>();
   const [selectedTags, setSelectedTags] = useState<MultiValue<Tags>>();
+  const [hideFilters, setHideFilters] = useState(true);
 
   const columnHelper = createColumnHelper<Listing>();
 
@@ -62,44 +70,77 @@ const Table: React.FC<TableProps> = ({ listings }) => {
   return (
     <>
       <section>
-        <div className="filters">
-          <div>
-            <input
-              type="checkbox"
-              id="only_remote"
-              onChange={() => setOnlyRemote((prevState) => !prevState)}
-            />
+        <div>
+          <button onClick={() => setHideFilters((prevState) => !prevState)}>
+            <Icon icon={FunnelIcon} size="md" className="text-blue-500 px-0" />
+            <span>Filter</span>
+          </button>
+          <div
+            className={`
+              absolute bg-white p-5 border shadow-lg transition z-10 rounded-lg ring-1 min-w-[350px]
+              ${hideFilters ? 'hidden' : ''}
+            `}
+          >
+            <div className="">
+              <input
+                type="checkbox"
+                id="only_remote"
+                className="mb-4"
+                onChange={() => setOnlyRemote((prevState) => !prevState)}
+              />
 
-            <label
-              htmlFor="only_remote"
-              className="ml-2 select-none cursor-pointer"
-            >
-              Only Remote
-            </label>
-          </div>
+              <label
+                htmlFor="only_remote"
+                className="ml-2 select-none cursor-pointer"
+              >
+                Only Remote
+              </label>
+            </div>
 
-          <div>
-            <Select
-              onChange={(option) => {
-                console.log('changed to:  ', option);
-                setSelectedProvider(option);
-              }}
-              isMulti
-              options={providers}
-              placeholder="Select providers to filter..."
-            />
-          </div>
+            <div>
+              <label htmlFor="tags_select">Providers:</label>
+              <Select
+                onChange={(option) => {
+                  console.log('changed to:  ', option);
+                  setSelectedProvider(option);
+                }}
+                isMulti
+                options={providers}
+                placeholder="Select providers to filter..."
+                className="mb-3 mt-1"
+              />
+            </div>
 
-          <div>
-            <Select
-              onChange={(option) => {
-                console.log('changed to:  ', option);
-                setSelectedTags(option);
-              }}
-              isMulti
-              options={tags}
-              placeholder="Select tags to filter..."
-            />
+            <div>
+              <label htmlFor="tags_select">Tags:</label>
+              <Select
+                id="tags_select"
+                onChange={(option) => {
+                  console.log('changed to:  ', option);
+                  setSelectedTags(option);
+                }}
+                isMulti
+                options={tags}
+                placeholder="Select tags to filter..."
+                className="mt-1"
+              />
+            </div>
+
+            <div className="mt-8 flex justify-between">
+              <Button size="xs" icon={ArrowPathIcon}>
+                {/* TODO pass loading */}
+                Apply Filters
+              </Button>
+
+              <Button
+                size="xs"
+                variant="secondary"
+                color="red"
+                icon={XMarkIcon}
+              >
+                Clear Filters
+              </Button>
+            </div>
           </div>
         </div>
 
